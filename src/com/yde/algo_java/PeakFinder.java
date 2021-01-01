f39ba0a796e6f5a313b3b64d35696a1c1ed554f2
yde773786 <yde773786@gmail.com>
2021-01-01 15:52:36 +0400

    Peak Finder completed.

--- src/com/yde/algo_java/PeakFinder.java
+++ src/com/yde/algo_java/PeakFinder.java
@@ -60,7 +60,7 @@ public class PeakFinder {
                     "Elapsed Time: " + (endTime - startTime));
 
             startTime = System.currentTimeMillis();
-            int[] ans_2 = updatedTwoDimPeakFinder(mat);
+            int[] ans_2 = updatedTwoDimPeakFinder(mat, 0, mat[0].length - 1);
             endTime = System.nanoTime();
             System.out.println("Peak Found:  (" + ans_2[0] + ", " + ans_2[1] + ")" + "\n" +
                     "Elapsed Time: " + (endTime - startTime));
@@ -68,6 +68,13 @@ public class PeakFinder {
         }
     }
 
+    /**
+     *Runs over the array until an element is found that
+     * satisfies the conditions for peak.
+     *
+     * @param row Input one dimensional array
+     * @return  Required peak
+     */
     private static int simpleOneDimPeakFinder(int[] row) {
         if(row.length > 1){
             if(row[0] >= row[1])
@@ -81,6 +88,18 @@ public class PeakFinder {
         return 0;
     }
 
+    /**
+     * Starts from midpoint of array. If an element on either
+     * side of the middle element is greater than it, a peak
+     * is guaranteed to be present on that half of the current
+     * array. Otherwise, the middle itself is the peak. Continue
+     * recursively with the target half.
+     *
+     * @param row Input one dimensional array
+     * @param start Beginning point of current array
+     * @param end End point of current array
+     * @return  Required peak
+     */
     private static int updatedOneDimPeakFinder(int[] row, int start, int end) {
         int mid = (end + start + 1) / 2;
         if(end - start > 1) {
@@ -94,6 +113,13 @@ public class PeakFinder {
         return row[end] > row[start] ? end : start;
     }
 
+    /**
+     * Runs over the 2d array until an element is found that
+     * satisfies the conditions for peak.
+     *
+     * @param mat 2 dimensional array
+     * @return Required peak
+     */
     private static int[] simpleTwoDimPeakFinder(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
@@ -115,8 +141,10 @@ public class PeakFinder {
                 for (int j = -1; j <= 1; j++) {
                     int temp_c = c_c + j;
                     if(0 <= temp_c && temp_c <= mat[0].length - 1
-                    && mat[temp_r][temp_c] > mat[c_r][c_c]) {
-                        return false;
+                    && mat[temp_r][temp_c] > mat[c_r][c_c]
+                    && (i == 0 || j == 0)) {// Removing this line makes a true peak finder,
+                        return false;       // Checking top right, top left, bottom right and
+                                            // bottom left as well.
                     }
                 }
             }
@@ -125,7 +153,47 @@ public class PeakFinder {
         return true;
     }
 
-    private static int[] updatedTwoDimPeakFinder(int[][] mat) {
-        return new int[]{0, 0};
+    /**
+     * Finds the maximum element in the middle column of the current 2d array.
+     * Checks if the elements adjacent (horizontally) to this maximum in the
+     * middle column are greater than it. If one is found, it is guaranteed
+     * that there would be a peak in that half. Continue recursively with the
+     * target half.
+     *
+     * @param mat 2 dimensional array
+     * @param start_col Beginning column of current 2 dimensional array
+     * @param end_col End column of current 2 dimensional array
+     * @return  Required peak
+     */
+    private static int[] updatedTwoDimPeakFinder(int[][] mat, int start_col, int end_col) {
+        int mid_col = (end_col - start_col + 1) / 2;
+
+        if(end_col - start_col > 1){
+            int mid_max = columnMaximum(mat, mid_col);
+            if(mat[mid_max][mid_col] < mat[mid_max][mid_col - 1])
+                updatedTwoDimPeakFinder(mat, start_col, mid_col);
+            else if(mat[mid_max][mid_col] < mat[mid_max][mid_col + 1])
+                updatedTwoDimPeakFinder(mat, mid_col, end_col);
+            else
+                return new int[]{mid_max, mid_col};
+        }
+        int start_max = columnMaximum(mat, start_col);
+        int end_max = columnMaximum(mat, end_col);
+
+        return mat[start_max][start_col] >= mat[start_max][end_col]
+        ? new int[]{start_max, start_col} : new int[]{end_max, end_col};
+    }
+
+    private static int columnMaximum(int[][] mat, int col){
+        int max = 0;
+
+        if(mat.length > 1) {
+            for (int i = 1; i < mat.length; i++) {
+                if (mat[i][col] > mat[max][col])
+                    max = i;
+            }
+        }
+
+        return max;
     }
 }
